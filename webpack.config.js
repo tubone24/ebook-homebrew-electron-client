cb8a8c8cdfed166a0932ecf95fda6276eb72e6eb
tubone24 <tubo.yyyuuu@gmail.com>
2019-10-30 00:38:19 +0900

    logo

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const main = {
+  mode: 'development',
+  target: 'electron-main',
+  entry: path.join(__dirname, 'src', 'index'),
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  module: {
+    rules: [{
+      test: /.ts?$/,
+      include: [
+        path.resolve(__dirname, 'src'),
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules'),
+      ],
+      loader: 'ts-loader',
+    },
+      {
+        test: /\.(jpg|png)$/,
+        include: [
+          path.resolve(__dirname, 'src/assets'),
+        ],
+        loader: 'file-loader?name=[name].[ext]',
+      }]
+  },
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/html/index.html'
+    })
+  ]
+};
+
+const renderer = {
+  mode: 'development',
+  target: 'electron-renderer',
+  entry: path.join(__dirname, 'src', 'renderer', 'index'),
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist', 'scripts')
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.css', '.ts', '.tsx']
+  },
+  module: {
+    rules: [{
+      test: /\.(tsx|ts)$/,
+      use: [
+        'ts-loader'
+      ],
+      include: [
+        path.resolve(__dirname, 'src'),
+        path.resolve(__dirname, 'node_modules'),
+      ],
+    }]
+  },
+};
+
+module.exports = [
+  main, renderer
+];
